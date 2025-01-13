a78c6e06a11e4dbe1b2274f4121a8cdf5f393c00
Veng TEA <veng.tea@allweb.com.kh>
2025-01-13 17:54:29 +0700

    five-commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AxiosExceptionFilter } from './exception/AxiosExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // Apply the custom exception filter globally
   app.useGlobalFilters(new AxiosExceptionFilter());
   await app.listen(4002);
